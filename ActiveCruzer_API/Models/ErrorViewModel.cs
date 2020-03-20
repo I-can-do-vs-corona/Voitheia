fd7d9e251cf0a43004537e9ea4a3ad333cbcdd86
Lukas Krappmann <lukekrappmann@outlook.com>
2020-03-20 23:56:16 +0100

    API commit

---
+++ ActiveCruzer_API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ActiveCruzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
