d70072595ff142484af024ffee90e9e112961359
Lukas Krappmann <lukekrappmann@outlook.com>
2020-03-21 19:18:20 +0100

    add db connection string support for configure services

--- Backend/ActiveCruzer/Startup.cs
+++ Backend/ActiveCruzer/Startup.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using ActiveCruzer.DAL.DataContext;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -33,12 +34,10 @@ namespace ActiveCruzer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            /// db connection
             services.AddDbContext<ACDatabaseContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("ActiveCruzerDB")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ACDatabaseContext>();
+
 
             services.AddControllersWithViews();
 
