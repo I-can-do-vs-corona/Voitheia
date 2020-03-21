6db646d8fb6e9139b28dad1343736aeacc97c7cb
Benedikt Bergmann <github@bb-webpromotion.de>
2020-03-21 18:57:56 +0100

    Adding stepper to create form

--- Frontend/voitheia-App/src/app/common/modules/material/material.module.ts
+++ Frontend/voitheia-App/src/app/common/modules/material/material.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import {MatInputModule} from '@angular/material/input';
+import {MatStepperModule} from '@angular/material/stepper';
 import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatInputModule,
-    MatSelectModule
+    MatStepperModule,
+    MatSelectModule,
+    MatRadioModule
   ],
   exports: [
     MatInputModule,
-    MatSelectModule
+    MatStepperModule,
+    MatSelectModule,
+    MatRadioModule
   ]
 })
 export class MaterialModule { }
