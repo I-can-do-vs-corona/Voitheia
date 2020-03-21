566fe7e47ae484c27f4cfb79b682bf48bdf08713
Andreas Ziegler <andreas.ziegler@acando.com>
2020-03-21 08:35:04 +0100

    Activate Swagger

--- Backend/ActiveCruzer/Startup.cs
+++ Backend/ActiveCruzer/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace ActiveCruzer
 {
@@ -25,11 +28,43 @@ namespace ActiveCruzer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            RegisterSwaggerGen(services);
+        }
+
+        private static void RegisterSwaggerGen(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Voithea API",
+                    Description = "Voll krasse Voithea api",
+                    TermsOfService = new Uri("https://voithea.org/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Gerd Wagner",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/Gerdi"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "I will geld",
+                        Url = new Uri("https://voithea.org/license"),
+                    }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            ConfigureSwaggerGen(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -40,6 +75,7 @@ namespace ActiveCruzer
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -62,7 +98,17 @@ namespace ActiveCruzer
                 endpoints.MapControllerRoute(
                     name: "authentication",
                     pattern: "{controller=Authentication}/{action=Index}/{id?}");
+            });
+        }
 
+        private static void ConfigureSwaggerGen(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
             });
         }
     }
