16942a9b549e9a87d46f20b8ec1b90d12ccba34c
Benedikt Bergmann <github@bb-webpromotion.de>
2020-03-23 16:04:11 +0100

    Fixing header

--- Frontend/voitheia-App/src/app/components/layout/header/header.component.ts
+++ Frontend/voitheia-App/src/app/components/layout/header/header.component.ts
@@ -8,28 +8,53 @@ import { NavigationService } from 'src/app/common/shared/services/navigation.ser
 })
 export class HeaderComponent implements OnInit {
 
+  stickyHeaderHeight = 70;
+  unstickyHeaderHeight = 100;
+  offset = 5;
+  scrollFactor = 1.5;
+
   constructor(public _navigationService: NavigationService) { }
 
   ngOnInit(): void {
+    
   }
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll(e) {
-     if (window.pageYOffset > 120) {
-       let headerElement = document.getElementById('header');
-       headerElement.classList.add('sticky');
-       headerElement.classList.add('sticky-top');
+    if (window.pageYOffset > (this.unstickyHeaderHeight + this.offset) && window.pageYOffset < (this.unstickyHeaderHeight + this.offset + (this.stickyHeaderHeight * this.scrollFactor))){
+      var calculatedTop = (window.pageYOffset - (this.unstickyHeaderHeight + this.offset + (this.stickyHeaderHeight * this.scrollFactor)));
 
-       let pageContentElement = document.getElementById('page-content');
-       pageContentElement.classList.add('marginTop');
-     } else {
-       let headerElement = document.getElementById('header');
-       headerElement.classList.remove('sticky'); 
-       headerElement.classList.remove('sticky-top'); 
+      let headerElement = document.getElementById('header');
+      headerElement.classList.add('sticky');
+      headerElement.setAttribute("style", "top:" + calculatedTop + "px");
 
-       let pageContentElement = document.getElementById('page-content');
-       pageContentElement.classList.remove('marginTop');
-     }
+      let tableHeaders = document.getElementsByClassName('mat-table-sticky');
+      for (let i = 0; i < tableHeaders.length; i++) {
+        const element = tableHeaders[i] as HTMLElement;
+        element.style.top = (this.stickyHeaderHeight + calculatedTop) + "px";
+      }
+    }
+    else if (window.pageYOffset >= (this.unstickyHeaderHeight + this.offset + (this.stickyHeaderHeight * this.scrollFactor))) {
+      let headerElement = document.getElementById('header');
+      headerElement.classList.add('sticky');
+      headerElement.removeAttribute("style");
+
+      let tableHeaders = document.getElementsByClassName('mat-table-sticky');
+      for (let i = 0; i < tableHeaders.length; i++) {
+        const element = tableHeaders[i] as HTMLElement;
+        element.style.top = this.stickyHeaderHeight + "px";
+      }
+    } else {
+      let headerElement = document.getElementById('header');
+      headerElement.classList.remove('sticky');
+      headerElement.removeAttribute("style");
+
+      let tableHeaders = document.getElementsByClassName('mat-table-sticky');
+      for (let i = 0; i < tableHeaders.length; i++) {
+        const element = tableHeaders[i] as HTMLElement;
+        element.style.top = "0px";
+      }
+    }
   }
 
 }
